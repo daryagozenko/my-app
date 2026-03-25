02f3deebd7c8976d92b45b096d89e24c17d927a4
Дарья <gozenko.dasha@bk.ru>
2026-03-25 10:38:36 +0400

    add base router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <header className="App-header">
+      <h1>Hello World!</h1>
+      <p>Мое первое React приложение на GitHub Pages</p>
+      <Link to="/about">О нас</Link>
+    </header>
+  );
+}
+
+function About() {
+  return (
+    <header className="App-header">
+      <h1>О нас</h1>
+      <p>Это страница информации о нашем приложении</p>
+      <Link to="/">На главную</Link>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-        <p>Мое первое React приложение на GitHub Pages</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
